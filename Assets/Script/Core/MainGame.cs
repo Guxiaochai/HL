e2f03cded9d2fa374a45da86d11663c81b20a702
DESKTOP-85ISO1E\Manny <410510921@qq.com>
2022-08-29 23:58:56 +0800

    1、新增文件夹 2、新增空场景 3、新增游戏入口相关Prefab和脚本

---
+++ Assets/Script/Core/MainGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("233");
+    }
+
+}

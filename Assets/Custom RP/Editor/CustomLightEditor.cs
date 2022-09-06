64a3091e40b7247d646e2a1d4e7b5cff69d3385e
EmmerHu <514252718@qq.com>
2022-09-06 14:50:45 +0800

    [Renderpipeline] 在inspector中添加对spot light的inner angle调整的slider

---
+++ Assets/Custom RP/Editor/CustomLightEditor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+[CanEditMultipleObjects]
+[CustomEditorForRenderPipeline(typeof(Light), typeof(CustomRenderPipelineAsset))]
+public class CustomLightEditor : LightEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if(!settings.lightType.hasMultipleDifferentValues &&
+            (LightType)settings.lightType.enumValueIndex == LightType.Spot)
+        {
+            settings.DrawInnerAndOuterSpotAngle();
+            settings.ApplyModifiedProperties();
+        }
+    }
+}

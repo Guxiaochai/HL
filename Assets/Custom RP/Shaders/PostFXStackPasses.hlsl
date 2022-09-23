78d85d96d845b33de53cac70f218dd5326ea44ff
EmmerHu <514252718@qq.com>
2022-09-23 17:49:08 +0800

    [Renderpipeline] 使用一个小技巧，在高斯vertical filter的同时可以进行bilinear filter

--- Assets/Custom RP/Shaders/PostFXStackPasses.hlsl
+++ Assets/Custom RP/Shaders/PostFXStackPasses.hlsl
@@ -60,13 +60,12 @@ float4 BloomHorizontalPassFragment(Varyings input) : SV_TARGET{
 float4 BloomVerticalPassFragment(Varyings input) : SV_TARGET{
     float3 color = 0.0;
 	float offsets[] = {
-		-4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0
+		-3.23076923, -1.38461538, 0.0, 1.38461538, 3.23076923
 	};
 	float weights[] = {
-		0.01621622, 0.05405405, 0.12162162, 0.19459459, 0.22702703,
-		0.19459459, 0.12162162, 0.05405405, 0.01621622
+		0.07027027, 0.31621622, 0.22702703, 0.31621622, 0.07027027
 	};
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < 5; i++){
         float offset = offsets[i] * GetSourceTexelSize().y;
         color += GetSource(input.screenUV + float2(0.0, offset)).rgb * weights[i];
     }

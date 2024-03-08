c882bbf779baddfb530618b8b21303c935dda950
EmmerHu <514252718@qq.com>
2024-03-08 13:46:39 +0800

    ignore

---
+++ Assets/Scripts/Test scripts/Emmer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Emmer", menuName = "ScriptableObjects/Emmer")]
+public class Emmer : ScriptableObject
+{
+    public string exampleData;
+    public int exampleNumber;
+}
